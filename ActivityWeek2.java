5a37d46cbeede1ef2afda4b2c87b9dae385c6a17
nicocoo22 <judeniccolo.espiritu@lorma.edu>
2026-01-20 12:05:53 +0800

    Activity1

---
+++ ActivityWeek2.java
@@ -0,0 +1,17 @@
+public class ActivityWeek2 {
+    public static void main(String [] args){
+        int [] theaterRow = new int[9];
+
+        theaterRow[3] = 1;
+
+        int availableSeats = 0;
+        for (int i = 1; i < theaterRow.length; i++){
+            System.out.println("Seat" + i +": " + theaterRow[i]);
+        
+            if( theaterRow [i] == 0);{
+            availableSeats++;
+        }
+             System.out.println("Available seats" );
+    } 
+    }
+}
